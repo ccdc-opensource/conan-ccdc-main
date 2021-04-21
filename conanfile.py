421f43bad4fbed063bcf5f40537e99a5620d0c28
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-04-21 16:18:20 +0000

    Updated conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,40 +1,57 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 
-class CcdcMainConan(ConanFile):
-    name = "ccdcmain"
-    # version = '1.0.0'
+class CCDCMainConan(ConanFile):
+    name = "ccdc-main"
+    version = '2021.0.0'
     description = "Dependencies of main repository in CCDC"
     homepage = "https://ccdc.cam.ac.uk/"
     url = "https://github.com/conan-io/conan-center-index"
     topics = ("conan", "chemistry", "crystallography")
     license = "Proprietary"
-    generators = "cmake_multi"
+    author = "Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>"
 
     settings = "os", "arch", "compiler", "build_type"
-            
+    no_copy_source = True
+
     def build_requirements(self):
-        self.build_requires("zlib/1.2.11")
-        self.build_requires("rapidjson/1.1.0")
+        if self.settings.os == 'Windows':
+            self.build_requires("7zip/19.00")
+
         self.build_requires("cmake/3.17.3")
-        self.build_requires("ninja/1.10.0")
-        self.build_requires("ccdcsqlite3/3.17.0")
         self.build_requires("gtest/1.8.1")
-        self.build_requires("openssl/1.1.1g")
-        self.build_requires("protobuf/3.11.4")
-        self.build_requires("gsoap/2.8.103")
-        self.build_requires("freetype/2.10.1")
-        self.build_requires("openscenegraph/3.6.3")
-        self.build_requires("cppad/20150000.9")
-        self.build_requires("fasta/36.3.8f")
-        self.build_requires("lexactivator/3.9.2")
-        self.build_requires("lexfloatclient/4.3.5")
-        self.build_requires("lexfloatserver/4.3.1")
-        self.build_requires("libxl/3.8.2.0")
-        self.build_requires("povray/3.7.0.8")
-        self.build_requires("installbuilder/20.4.0")
-        self.build_requires("rstatistics/2.11.1")
-        self.build_requires("swig/4.0.1")
-        self.build_requires("inchi/1.04")
+        self.build_requires("installbuilder/20.12.0")
+        self.build_requires("ninja/1.10.1")
+        self.build_requires("swig/4.0.2")
+
+    def requirements(self):
+        if self.settings.os != 'Windows':
+            self.requires("fontconfig/2.13.93")
+
+        self.requires("ccdcboost/1.73.0")
+        self.requires("ccdcsqlite3/3.17.0")
+        self.requires("cppad/20150000.9")
+        self.requires("cryptopp/8.2.0@bincrafters/stable")
+        self.requires("csdprotobufs/1.0.125")
+        self.requires("fasta/36.3.8f")
+        self.requires("gsoap/2.8.113")
+        self.requires("inchi/1.04")
+        self.requires("lexactivator/3.12.1")
+        self.requires("lexfloatclient/4.3.10")
+        self.requires("lexfloatserver/4.3.10")
+        self.requires("libarchive/3.5.1")
+        self.requires("libxl/3.8.2.0")
+        self.requires("mariadb-connector-c/3.1.11")
+        self.requires("openscenegraph/3.6.3")
+        self.requires("openssl/1.1.1k")
+        self.requires("povray/3.7.0.8")
+        self.requires("range-v3/0.11.0")
+        self.requires("rapidjson/1.1.0")
+        self.requires("rstatistics/2.11.1")
+        self.requires("zlib/1.2.11")
+        self.requires("zstd/1.4.9")
